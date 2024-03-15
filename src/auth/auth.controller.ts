ab29797a32842ca594117d1a058b6b8fd387764f
jieun615 <je060105@gmail.com>
2024-03-16 00:30:40 +0900

    데코레이터 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import { Controller, Request, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiExtraModels, ApiTags, getSchemaPath } from '@nestjs/swagger';
 import { SigninReqDto, SignupReqDto } from './dto/req.dto';
+import { SignupResDto } from './dto/res.dto';
 
 @ApiTags('Auth')
+@ApiExtraModels(SignupResDto)
 @Controller('api/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiCreatedResponse({
+    schema: {
+      allOf: [{ $ref: getSchemaPath(SignupResDto) }],
+    },
+  })
   @Post('signup')
   async signup(@Body() signupReqDto: SignupReqDto) {
     return this.authService.signup('email', 'password');
