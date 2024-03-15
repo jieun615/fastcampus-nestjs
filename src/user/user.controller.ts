421bd9bee098be6ebd275415fb9d059814822142
jieun615 <je060105@gmail.com>
2024-03-16 01:26:01 +0900

    jwt 인증 기능 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
-import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { FindUserReqDto } from './dto/req.dto';
 import { PageReqDto } from 'src/common/dto/req.dto';
 import { ApiGetItemsResponse, ApiGetResponse } from 'src/common/decorator/swagger.decorator';
 import { FindUserResDto } from './dto/res.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @ApiTags('User')
 @ApiExtraModels(FindUserReqDto, FindUserReqDto, FindUserResDto)
@@ -12,8 +13,10 @@ import { FindUserResDto } from './dto/res.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiBearerAuth()
   @ApiGetItemsResponse(FindUserResDto)
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll(@Query() { page, size }: PageReqDto) {
     return this.userService.findAll();
   }
