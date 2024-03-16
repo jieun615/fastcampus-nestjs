a0408392d4b8c1d048187704d0c1756bafa57aaf
jieun615 <je060105@gmail.com>
2024-03-16 17:09:49 +0900

    Authorization 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiBearerAuth, ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { FindUserReqDto } from './dto/req.dto';
 import { PageReqDto } from 'src/common/dto/req.dto';
 import { ApiGetItemsResponse, ApiGetResponse } from 'src/common/decorator/swagger.decorator';
 import { FindUserResDto } from './dto/res.dto';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { User, UserAfterAuth } from 'src/common/decorator/user.decorator';
+import * as userDecorator from 'src/common/decorator/user.decorator';
+import { Roles } from 'src/common/decorator/role.decorator';
+import { Role } from './enum/user.enum';
 
 @ApiTags('User')
 @ApiExtraModels(FindUserReqDto, FindUserReqDto, FindUserResDto)
@@ -16,8 +17,9 @@ export class UserController {
 
   @ApiBearerAuth()
   @ApiGetItemsResponse(FindUserResDto)
+  @Roles(Role.ADMIN)
   @Get()
-  findAll(@Query() { page, size }: PageReqDto, @User() user: UserAfterAuth) {
+  findAll(@Query() { page, size }: PageReqDto, @userDecorator.User() user: userDecorator.UserAfterAuth) {
     return this.userService.findAll();
   }
 
