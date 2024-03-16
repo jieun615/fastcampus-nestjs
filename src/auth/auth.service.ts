4ca2ddfddfde3a5b8119cc1bd73c99e987a25d5e
jieun615 <je060105@gmail.com>
2024-03-16 16:00:45 +0900

    refreshToken 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,17 @@
 import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
 import { UserService } from 'src/user/user.service';
+import { RefreshToken } from './entity/refresh-token.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService, private jwtService: JwtService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+    @InjectRepository(RefreshToken) private refreshTokenRepository: Repository<RefreshToken>
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     return null;
@@ -23,8 +30,27 @@ export class AuthService {
 
     const isMatch = password == user.password;
     if (!isMatch) throw new UnauthorizedException();
+
+    const refreshToken = this.generateRepositoryToken(user.id);
+    await this.createRefreshTokenUsingUser(user.id, refreshToken);
     return {
       accessToken: this.jwtService.sign({ sub: user.id }),
+      refreshToken,
     };
+  }
+
+  private generateRepositoryToken(userId: string) {
+    const payload = { sub: userId, tokenType: 'refresh' };
+    return this.jwtService.sign(payload, { expiresIn: '30d' });
+  }
+
+  private async createRefreshTokenUsingUser(userId: string, refreshToken: string) {
+    let refreshTokenEntity = await this.refreshTokenRepository.findOneBy({ user: { id: userId } });
+    if (refreshTokenEntity) {
+      refreshTokenEntity.token = refreshToken;
+    } else {
+      refreshTokenEntity = this.refreshTokenRepository.create({ user: { id: userId }, token: refreshToken });
+    }
+    await this.refreshTokenRepository.save(refreshTokenEntity);
   }
 }
